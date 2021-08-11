e9f5e063e2126c4859ca652d86aed43157e4015f
Adinda Budi Kusuma Putra <adindabudi@Adindas-MacBook-Pro.local>
2021-08-11 16:50:13 +0700

    Add index endpoint API and simple input validation

--- fastapi_addition/main.py
+++ fastapi_addition/main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
+@app.get("/")
+def index():
+    return "Simple Addition API"
+
 @app.get("/addition")
-def addition(a: int = 0, b: int = 0):
+def addition(a: int = Query(None), b: int = Query(None)):
+    if not a or not b:
+        return {
+            "status": "error",
+            "message": "Require 'a' and 'b' query parameter and must be integer"
+        }
     return {"result": a+b}
